ca5f07fe236b8e42fc1dfb5fc714b7d9c7b90ec1
therealmensumit <sk03950@gmail.com>
2023-01-01 20:42:14 +0530

    changes

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(function(){
+	$(".typed").typed({
+		strings: ["Web Designer", "Web Developer", "Full Stack Developer", "UI/UX Designer"],
+		stringsElement: null,
+		typeSpeed: 30,
+		startDelay: 1000,
+		backSpeed: 20,
+		backDelay: 500,
+		loop: true,
+        infinite: true,
+		contentType: 'html',
+        // showCursor: false
+	});
+});
